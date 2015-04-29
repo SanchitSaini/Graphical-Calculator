9a66557d3a54b4703501750e9c3636496d45fbd6
Pranav Chadha <pranavchadha97@gmail.com>
2015-04-30 02:06:58 +0530

    Error Module

---
+++ src/Calculator/ErrorModule.java
@@ -0,0 +1,16 @@
+package Calculator;
+
+/**
+ * Created by Pranav on 30-04-2015.
+ */
+
+import javax.swing.*;
+
+public class ErrorModule {
+
+    public static void displayError(String s)
+    {
+        JOptionPane.showMessageDialog(null, s, "Error", JOptionPane.ERROR_MESSAGE);
+
+    }
+}
